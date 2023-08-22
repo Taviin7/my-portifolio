4c2450ebe7c4d4565e930d58a24d0e3d6ddc8185
Taviin7 <otaviorl711@gmail.com>
2023-08-22 16:20:51 -0300

    Portifolio 100% "Pronto"

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import { FaGithubSquare, FaInstagramSquare, FaLinkedin, FaMailBulk } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <footer>
-        <div>mailto:otaviorl711@gmail.com</div>
+    <footer id='Footer'>
+      <div className='main-container-footer'>
+        <ul className='list-icon-footer'>
+          <a href='https://github.com/Taviin7' target="_blank"><FaGithubSquare size={40} /></a>
+          <a href='https://www.linkedin.com/in/otavio-rodrigues-lago-601355257/' target="_blank"><FaLinkedin size={40} /></a>
+          <a href='mailto:otaviorl711@gmail.com' target="_blank"><FaMailBulk size={40} /></a>
+          <a href='https://www.instagram.com/taviin9_/' target="_blank"><FaInstagramSquare size={40} /></a>
+        </ul>
+        <div className='container-end-footer'>
+          <a href='#Home'><h1 className='otavio-footer'>Otavio</h1><h1 className='lago-footer'>Lago</h1></a>
+        </div>
+      </div>
     </footer>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,15 +3,9 @@ import './Home.css';
 import Astronauta from './imgs/astrounata.png'
 
 export default function Home() {
-
     return (
         <section className='Home' id='Home'>
-
-            {/* Animation */}
             <div class="area" >
-                
-
-
                 {/* Header */}
                 <header id="header">
                     <nav>
@@ -19,7 +13,7 @@ export default function Home() {
                             <a href='#Home' className='ancora'>Home</a>
                             <a href='#Projetos' className='ancora'>Projetos</a>
                             <a href='#Skills' className='ancora'>Skills</a>
-                            <a href='#About' className='ancora'>Contato</a>
+                            <a href='#Footer' className='ancora'>Contato</a>
                         </ul>
                     </nav>
                 </header>
@@ -31,7 +25,6 @@ export default function Home() {
                         <h3>Desenvolvedor Full-Stack.</h3>
                         <p>Sou Técnico em Informática com ênfase em Desenvolvimento de Sistemas, e estou constantemente buscando ampliar mais meus conhecimentos sobre programação.
                             Tenho como objetivo evoluir tanto pessoalmente, como profissionalmente, aproveitando ao máximo cada oportunidade que surgir em minha carreira, e, claro, esforço e dedicação é o quê não falta!</p>
-
                     </div>
                     <div className='container-astronauta'>
                         <img src={Astronauta} />
